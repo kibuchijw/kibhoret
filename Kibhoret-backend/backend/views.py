90970ee9764a5d9ef6102a0b1826e54604f99345
Jukunye <jukunyes@gmail.com>
2024-02-01 16:40:53 +0300

    deleted some views

--- Kibhoret-backend/backend/views.py
+++ Kibhoret-backend/backend/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import GateEntry
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello backend")
-
-def truck_list(request):
-    trucks = GateEntry.objects.all()
-    output = ', '.join([t.truck_registration for t in trucks])
-    return HttpResponse(output)
 
 def weigh1(request):
     return HttpResponse('Weigh your truck')
@@ -23,7 +17,5 @@ def offloading(request):
 def weigh2(request):
     return HttpResponse('Finding second weight to calculate net load weight')
 
-def detail(request, gate_id):
-    truck = get_object_or_404(GateEntry, pk=gate_id) # could also use id in place of pk
-    reg_no = truck.truck_registration
-    return HttpResponse(f"Registration Number of Truck {gate_id}: {reg_no}")
+def trucks(request):
+    return HttpResponse('We shall now offload your tanker')
