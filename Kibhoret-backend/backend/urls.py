eb14efbd4f09800dfbdf1e2f62c27c3541b379f5
Jukunye <jukunyes@gmail.com>
2024-02-01 16:39:01 +0300

    created api urls

--- Kibhoret-backend/backend/urls.py
+++ Kibhoret-backend/backend/urls.py
@@ -5,12 +5,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), #best practice to name url endpoints
-    path('gate/', views.truck_list),
+    # path('gate/', views.truck_list),
     path('weighbridge-in/', views.weigh1),
     path('quality-control/', views.qualitycontrol),
     path('tankfarm/', views.offloading),
     path('weighbridge-out/', views.weigh2),
 
-    path('gate/<int:gate_id>/', views.detail),
+    # path('gate/<int:gate_id>/', views.detail),
+    path('trucks/', views.trucks)
     
 ]
