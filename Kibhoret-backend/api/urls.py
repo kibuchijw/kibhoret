4ee516529520130a9800a54524e14c6d9e736697
Jukunye <jukunyes@gmail.com>
2024-02-05 14:57:48 +0300

    add single truck url

--- Kibhoret-backend/api/urls.py
+++ Kibhoret-backend/api/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     # path('trucks/', views.Trucks.as_view()),
     path('trucks/', views.Trucks.as_view(), name='trucks'),
-    path('generalinfo/', views.GeneralInfos.as_view()),
+    path('truck/<int:pk>/', views.Truck.as_view()),
+    # path('generalinfo/', views.GeneralInfos.as_view()),
     path('tankfarm/', views.Tankfarm.as_view()),
 
     path('weighbridge/in/', views.WeighbridgeIn.as_view()),
