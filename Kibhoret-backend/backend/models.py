38f47b039fdfe2df5082c9c2d1e8c5e8731e7efa
Jukunye <jukunyes@gmail.com>
2024-02-06 07:10:36 +0300

    Json response

--- Kibhoret-backend/backend/models.py
+++ Kibhoret-backend/backend/models.py
@@ -3,11 +3,12 @@ from django.utils import timezone
 
 # Create your models here.
 
+
 class GeneralInfo(models.Model):
     delivery_number = models.CharField(max_length=20)
     loading_date = models.DateField()
     FUEL_CHOICES = [('empty', 'Empty'), ('half', 'Half'), ('full', 'Full')]
-    fuel_gauge = models.CharField(max_length=10, choices=FUEL_CHOICES)  
+    fuel_gauge = models.CharField(max_length=10, choices=FUEL_CHOICES)
     WATER_CHOICES = [('empty', 'Empty'), ('half', 'Half'), ('full', 'Full')]
     water_reservoir = models.CharField(max_length=10, choices=WATER_CHOICES)
     number_of_seals = models.IntegerField()
@@ -18,10 +19,13 @@ class GeneralInfo(models.Model):
     time_out = models.DateTimeField(default=timezone.now)
     officer_name = models.CharField(max_length=100)
 
-    #other methods
+    class Meta:
+        verbose_name_plural = "General Info"
+
+    # other methods
     def __str__(self):
         return " GateEntry"
-    
+
 
 class WeighBridgeIn(models.Model):
     delivery_number = models.CharField(max_length=20)
@@ -33,11 +37,11 @@ class WeighBridgeIn(models.Model):
 
     def __str__(self):
         return " WB-In"
-    
 
 
 class QualityControl(models.Model):
-    sample_type = models.CharField(max_length=10, choices=[('CPO', 'CPO'), ('CPKO', 'CPKO'), ('stearin', 'Stearin'), ('water', 'Water'), ('sludge', 'Sludge'), ('caustic', 'Caustic')])
+    sample_type = models.CharField(max_length=10, choices=[('CPO', 'CPO'), ('CPKO', 'CPKO'), (
+        'stearin', 'Stearin'), ('water', 'Water'), ('sludge', 'Sludge'), ('caustic', 'Caustic')])
     sample_temperature = models.DecimalField(max_digits=5, decimal_places=2)
     notes = models.TextField()
     inspector_name = models.CharField(max_length=100)
@@ -68,7 +72,7 @@ class OffloadingBay(models.Model):
 
     def __str__(self):
         return " offloadBay"
-    
+
 
 class WeighBridgeOut(models.Model):
     last_weight = models.DecimalField(max_digits=10, decimal_places=2)
@@ -79,17 +83,22 @@ class WeighBridgeOut(models.Model):
 
     def __str__(self):
         return " WB-OUT"
-    
+
+
 class Truck(models.Model):
     driver = models.CharField(max_length=50)
     cab_plate = models.CharField(max_length=20)
     trailer_plate = models.CharField(max_length=20)
-    general_info = models.ForeignKey(GeneralInfo, on_delete=models.CASCADE, null=True, related_name='truck')
-    weighbridge_in = models.ForeignKey(WeighBridgeIn, on_delete=models.CASCADE, null=True, related_name='truck')
-    quality_control = models.ForeignKey(QualityControl, on_delete=models.CASCADE, null=True, related_name='truck')
-    tankfarm = models.ForeignKey(OffloadingBay, on_delete=models.CASCADE, null=True, related_name='truck')
-    weighbridge_out = models.ForeignKey(WeighBridgeOut, on_delete=models.CASCADE, null=True, related_name='truck')
-
+    general_info = models.ForeignKey(
+        GeneralInfo, on_delete=models.CASCADE, null=True, related_name='truck')
+    weighbridge_in = models.ForeignKey(
+        WeighBridgeIn, on_delete=models.CASCADE, null=True, related_name='truck')
+    quality_control = models.ForeignKey(
+        QualityControl, on_delete=models.CASCADE, null=True, related_name='truck')
+    tankfarm = models.ForeignKey(
+        OffloadingBay, on_delete=models.CASCADE, null=True, related_name='truck')
+    weighbridge_out = models.ForeignKey(
+        WeighBridgeOut, on_delete=models.CASCADE, null=True, related_name='truck')
 
     def __str__(self):
         return self.cab_plate
