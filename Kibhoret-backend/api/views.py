5c8eec9db4b47334d180880c56238501ea492426
Shadkoech <shadrackipruto@gmail.com>
2024-02-07 23:22:44 +0300

    Cleared out commented lines

--- Kibhoret-backend/api/views.py
+++ Kibhoret-backend/api/views.py
@@ -7,9 +7,9 @@ from backend.models import Truck as TruckModel
 from .serializers import *
 from django.http import JsonResponse
 
-
 from django.shortcuts import get_object_or_404
 from rest_framework.exceptions import NotFound, ValidationError
+
 
 class Truck(APIView):
     def get(self, request, pk):
@@ -49,54 +49,6 @@ class Trucks(APIView):
         else:
             error_message = "Validation Error in POST request. Please check the following fields:"
             raise ValidationError({error_message: serializer.errors})
-
-
-
-
-
-
-# Create your views here.
-# class Truck(APIView):
-#     def get(self, request, pk):
-#         truck = TruckModel.objects.get(pk=pk)
-#         serializer = TruckSerializer(truck)
-#         return JsonResponse(serializer.data)
-
-#     def delete(self, request, pk):
-#         truck = TruckModel.objects.get(pk=pk)
-#         truck.delete()
-#         return JsonResponse(status=status.HTTP_204_NO_CONTENT)
-
-#     def put(self, request, pk):
-#         truck = TruckModel.objects.get(pk=pk)
-#         serializer = TruckSerializer(truck, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return   JsonResponse(serializer.data)
-#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class Trucks(APIView):
-#     def get(self, request):
-#         trucks = TruckModel.objects.select_related().all()
-#         serialized_trucks = TruckSerializer(trucks, many=True)
-#         total_trucks = len(serialized_trucks.data)
-#         all_trucks = serialized_trucks.data
-#         return JsonResponse({"total_trucks": total_trucks, "all_trucks": all_trucks})
-
-#     def post(self, request):
-#         serializer = TruckSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
-#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class GeneralInfos(APIView):
-#     def get(self, request):
-#         infos = GeneralInfo.objects.prefetch_related('truck').all()
-#         serialized_infos = GeneralInfoSerializer(infos, many=True)
-#         return Response(serialized_infos.data)
 
 
 class WeighbridgeIn(APIView):
