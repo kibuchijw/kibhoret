602950c07bcbb22b172a7ae81e1c81691761729c
kibuchijw <kibuchijw@gmail.com>
2024-02-16 11:03:23 +0300

    Cleanup

--- Kibhoret-backend/api/models.py
+++
@@ -1,9 +0,0 @@
-# from django.db import models
-# from tastypie.resources import ModelResource
-# from backend.models import GateEntry
-
-# # Create your models here.
-# class GateEntryResource(ModelResource):
-#     class Meta:
-#         queryset = GateEntry.objects.all() #returns a query that will be loaded at a given time in the future
-#         resource_name = 'gate'
